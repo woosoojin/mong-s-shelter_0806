0302e7a6150f37e615d2ca7f31acc389688bc1ee
woosoojin <wsj1998@naver.com>
2019-07-19 23:56:50 +0900

    last

--- project/post/models.py
+++ project/post/models.py
@@ -8,3 +8,19 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Question(models.Model):
+    title2 = models.CharField(max_length = 100)
+    description2 = models.CharField(max_length = 10000)
+
+    def __str__(self):
+        return self.title2
+
+class Adopting(models.Model):
+    name = models.CharField(max_length=255)
+    image2 = models.ImageField(upload_to='images/')
+    description3 = models.CharField(max_length=10000)
+
+    def __str__(self):
+        return self.name
--- project/post/views.py
+++ project/post/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
-from .models import Post
+from .models import Post, Question, Adopting
 
 # Create your views here.
 def post(request):
     posts = Post.objects
     return render(request, 'post.html', {'posts':posts})
+
+def QA(request):
+    questions = Question.objects
+    return render(request, 'q&a.html', {'questions':questions})
+
+def adopting(request):
+    adoptings = Adopting.objects
+    return render(request, 'adopting.html', {'adoptings':adoptings})
